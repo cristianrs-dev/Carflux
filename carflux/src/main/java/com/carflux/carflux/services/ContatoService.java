67ea7835fe13f4541bda86e68a011fc96e4bdcb0
cristianrs-dev <cris.tecwordd@gmail.com>
2025-06-14 17:06:35 -0400

    feat: ContatoService implementada junto com buscarContatos e buscarContatosPeloId

---
+++ carflux/src/main/java/com/carflux/carflux/services/ContatoService.java
@@ -0,0 +1,28 @@
+package com.carflux.carflux.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carflux.carflux.model.Contato;
+import com.carflux.carflux.repository.ContatoRepository;
+
+@Service
+public class ContatoService {
+
+	@Autowired
+	private ContatoRepository repository;
+	
+	public List<Contato> buscarContatos(){
+		
+		return repository.findAll();
+		
+	}
+	
+	public Contato buscarContatoPeloId(Integer id) {
+		
+		return repository.findById(id).orElse(null);
+		
+	}
+}
