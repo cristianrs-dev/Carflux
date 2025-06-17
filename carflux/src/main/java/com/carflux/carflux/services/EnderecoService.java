bdf3b0d816e261aebe186859a5020f5472a2c84c
cristianrs-dev <cris.tecwordd@gmail.com>
2025-06-17 18:44:16 -0400

    feat:buscarEnderecoPeloId implementada

---
+++ carflux/src/main/java/com/carflux/carflux/services/EnderecoService.java
@@ -0,0 +1,27 @@
+package com.carflux.carflux.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carflux.carflux.model.Endereco;
+import com.carflux.carflux.repository.EnderecoRepository;
+
+@Service
+public class EnderecoService {
+	
+	@Autowired
+	EnderecoRepository repository;
+	
+	public List<Endereco> ListarTodosOsEnderecos(){
+		
+		return repository.findAll();
+		
+	}
+	
+	public Endereco buscarEnderecoPeloId(Integer id) {
+		Endereco endereco = repository.findById(id).orElse(null);
+			return endereco;
+	}
+}
