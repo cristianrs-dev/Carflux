a488c18fe54c9a2447ec76d8bcf4e31a621f1139
cristianrs-dev <cris.tecwordd@gmail.com>
2025-06-19 12:37:46 -0400

    refatoramento do clienteService

--- carflux/src/main/java/com/carflux/carflux/repository/ContatoRepository.java
+++ carflux/src/main/java/com/carflux/carflux/repository/ContatoRepository.java
@@ -1,9 +1,13 @@
 package com.carflux.carflux.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.carflux.carflux.model.Contato;
 
 public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+	
+	public Optional<Contato> findyByClienteCodigoCliente(Integer idCliente);
 	
 }
