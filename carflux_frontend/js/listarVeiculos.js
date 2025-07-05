3889746e732b241b0e51bc0df9a5b96b7263dbe6
cristianrs-dev <cris.tecwordd@gmail.com>
2025-07-04 21:19:48 -0400

    implementado tratamento de erros

--- carflux_frontend/js/listarVeiculos.js
+++ carflux_frontend/js/listarVeiculos.js
@@ -1,14 +1,10 @@
+import  {formatarPreco} from "../js/utils.js"; // Função para formatar preço como moeda brasileira
+import { listarVeiculos, buscarVeiculoPeloId,atualizarVeiculo,deletarVeiculo } from "./api/veiculo.service.js";
+import { closeBtnFechar,closeBtnModal,showModal } from "./components/modal.js";
 
-  // Função para formatar preço como moeda brasileira
-import  {formatarPreco} from "../js/utils.js";
-import { listarVeiculos } from "./api/veiculo.service.js";
-import { closeBtnFechar } from "./components/modal.js";
-import { closeBtnModal } from "./components/modal.js";
-import { showModal } from "./components/modal.js";
-import { buscarVeiculoPeloId } from "./api/veiculo.service.js";
 
   // Função que busca os veículos e popula a tabela
-  async function carregarVeiculos() {
+  async function preencherTabelaComVeiculos() {
     try {
      
       const veiculos = await listarVeiculos()
@@ -32,41 +28,129 @@ import { buscarVeiculoPeloId } from "./api/veiculo.service.js";
       });
     } catch (erro) {
       console.error(erro);
-      alert('Erro ao carregar os veículos.');
+      alert('Erro ao carregar os veículos.',erro);
     }
   }
-  
-closeBtnModal()
-closeBtnFechar()
 
+
+//função para settar os campos do form editar veículo
 document.addEventListener("click", async function (event) {
   if (event.target.classList.contains("editar")) {
-    const id = event.target.dataset.id
-   const veiculo = await buscarVeiculoPeloId(id)
-   console.log(veiculo.codigoVeiculo)
-   document.querySelector("#codigoVeiculo").value = veiculo.codigoVeiculo
-   document.querySelector("#marca").value = veiculo.marca
-   document.querySelector("#modelo").value = veiculo.modelo
-   document.querySelector("#ano").value = veiculo.ano
-   document.querySelector("#cor").value = veiculo.cor
-   document.querySelector("#km").value = veiculo.km
-   document.querySelector("#combustivel").value = veiculo.combustivel
-   document.querySelector("#cambio").value = veiculo.cambio
-   document.querySelector("#preco").value = veiculo.preco
+    try {
+      
+      const id = event.target.dataset.id
+      const veiculo = await buscarVeiculoPeloId(id)
+
+      if(!veiculo){
+        throw new Error("veículo não encontrado!")
+      }
+
+      document.querySelector("#codigoVeiculo").value = veiculo.codigoVeiculo
+      document.querySelector("#marca").value = veiculo.marca
+      document.querySelector("#modelo").value = veiculo.modelo
+      document.querySelector("#ano").value = veiculo.ano
+      document.querySelector("#cor").value = veiculo.cor
+      document.querySelector("#km").value = veiculo.km
+      document.querySelector("#combustivel").value = veiculo.combustivel
+      document.querySelector("#cambio").value = veiculo.cambio
+      document.querySelector("#preco").value = veiculo.preco
+
+   //função para exibir o modal
     showModal();
+
+    } catch (error) {
+      console.log(`erro ao carregar dados ${error}`)
+      alert("Não foi possível carregar os dados do veículo. Tente novamente.");
+    }
+
   }
+
 });
 
+                                //Atualiar registro do veículo
 document.addEventListener("click", async function(event) {
+  
   if(event.target.classList.contains("salvar")){
-   const form = document.querySelector("#formVeiculo")
-   const formData = new FormData(form)
-   const dadosVeiculo = Object.fromEntries(formData.entries())
-  }
+    try {
+
+      const form = document.querySelector("#formVeiculo")
+      if(!form) throw new Error("formulário não encontrado!")
+
+      const formData = new FormData(form)
+      const dadosVeiculo = Object.fromEntries(formData.entries())
+
+      if(!dadosVeiculo.codigoVeiculo){
+        throw new Error(`${dadosVeiculo.codigoVeiculo} está vazio ou indefinido`)
+      }
+
+      const idVeiculo = parseInt(dadosVeiculo.codigoVeiculo)
+
+      if(isNaN(idVeiculo)){
+        throw new Error(`${idVeiculo} não é um número válido!`)
+      }
+
+      const resposta = await atualizarVeiculo(idVeiculo,dadosVeiculo)
+      
+      if(resposta.ok){
+        alert("registro atualizado com sucesso!")
+        
+      }else{
+        const erroText = await resposta.text()
+        throw new Error(`erro ao salvar o registro ${resposta.status} - ${erroText}`)
+      }
+      
+    } catch (error) {
+      console.log(`erro ao buscar os dados do form ${error}`)
+      alert("Erro ao salvar os dados do veículo. Verifique os campos e tente novamente.");
+    }
+      
+    }
+  
 })
 
+              //DELETAR REGISTRO
+document.addEventListener("click", async function(event){
+  if(event.target.classList.contains("apagar")){
+    try {
+      
+      const idVeiculo = event.target.dataset.id
+      if(isNaN(idVeiculo)) throw new Error("não é um valor válido")
+      
+    const resposta = await deletarVeiculo(idVeiculo)
+
+    if(resposta.ok){
+
+      alert("Registro apagado com sucesso!")
+      preencherTabelaComVeiculos()
+      
+    }else if(resposta.status === 400){
+
+      alert("verifique os dados do formulário")
+      
+
+    }else if(resposta.status === 404){
+
+      alert("registro não encontrado")
+
+    }else {
+      const erroText = resposta.text()
+      throw new Error(`erro ao pagar o resgitro ${resposta.status} - ${erroText}`)
+    }
+
+    } catch (error) {
+
+      console.log(`não foi possível apagar os dados do veículo ${error}`)
+
+    }
+      
+  }
+
+})
+
+closeBtnModal()
+closeBtnFechar()
   // Chama a função quando a página carregar
-  window.addEventListener('DOMContentLoaded', carregarVeiculos);
+  window.addEventListener('DOMContentLoaded', preencherTabelaComVeiculos);
 
 
     
